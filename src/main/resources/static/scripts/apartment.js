a5a8649a94e45b807966b1cce9b88164150330c2
ariannatr <sdi1300179@di.uoa.gr>
2017-09-29 04:10:41 +0300

    view apartment's position in google_map

--- src/main/resources/static/scripts/apartment.js
+++ src/main/resources/static/scripts/apartment.js
@@ -17,23 +17,3 @@ function back(){
     $("#yourtext1").show();
 }
 
-$('#search_country').change(function() {
-    $('#country').val($('#search_country').val());
-});
-
-$('#search_town').change(function() {
-    $('#town').val($('#search_town').val());
-});
-
-$('#search_area').change(function() {
-    $('#area').val($('#search_area').val());
-});
-
-$('#search_address').change(function() {
-    $('#address').val($('#search_address').val());
-});
-
-$('#search_address_number').change(function() {
-    $('#address_number').val($('#search_address_number').val());
-});
-
